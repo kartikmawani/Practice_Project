26722a32ad4bac9f4d2b99dfcdccf6dd1543ef45
kartikmanwani <kartikmanwani2006@gmail.com>
2026-09-05 21:49:53 +0530

    api endpoind are added

--- backend/src/Router/router.ts
+++ backend/src/Router/router.ts
@@ -3,3 +3,5 @@ export const router=express.Router({caseSensitive:true,strict:true})
 //post is recommended for both as  login is saving credentials too
 router.post('/login',Authentication,loginController);
 router.post('/reqister',RegisterController);
+router.post('/task',Authentication,rateLimitController)
+router.post('/jobs',Authentication,microservie1)
